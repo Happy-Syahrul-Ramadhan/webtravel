dc9060dd24b3fb27d728506d2685f127bfeff0ef
datasyahrul <70791897+syahrul2003@users.noreply.github.com>
2023-01-24 16:37:57 +0700

    update js

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+//animasi form login
+const inputs = document.querySelectorAll(".input");
+
+
+function addcl() {
+	let parent = this.parentNode.parentNode;
+	parent.classList.add("focus");
+}
+
+function remcl() {
+	let parent = this.parentNode.parentNode;
+	if (this.value == "") {
+		parent.classList.remove("focus");
+	}
+}
+
+
+inputs.forEach(input => {
+	input.addEventListener("focus", addcl);
+	input.addEventListener("blur", remcl);
+});
+
 // preloader
 $(window).bind("load", function () {
 	$(".preloader").fadeOut();
@@ -105,4 +127,5 @@ document.querySelector('.append-2-slides').addEventListener('click', function (e
 	]);
 });
 
-//banner slider
+
+
